3f605bdacbb32290db86c80d7a992b8414d2f053
Aukerman <aukermaa@pllimsksparky3.mskcc.org>
2022-02-16 20:09:22 -0500

    Prototype finished

--- waystation/app.py
+++ waystation/app.py
@@ -1,52 +1,93 @@
 from flask import Flask, request, jsonify
 import click
 
-app = Flask(__name__)
+import os, logging
 
 import pandas as pd
-
-root_data_dir = '/tmp/data/tables'
-
-import os
 import threading
+
+from logging.config import dictConfig
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] [%(name)s] %(levelname)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+
+logger = logging.getLogger('waystation')
+logger.info("Starting luna waystation...")
+
+root_data_dir = 'staging/waystation/tables'
+
+s3_access_key = os.environ['S3_ACCESS_KEY']
+s3_secret_key = os.environ['S3_SECRET_KEY']
+s3_endpoint   = os.environ['S3_ENDPOINT']
+s3_host       = s3_endpoint.split(':')[0]
+
+os.system(f"echo | openssl s_client -servername {s3_host} -connect {s3_endpoint} |  sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /certificate.crt")
+
+os.system("cat /certificate.crt")
+
+os.environ['SSL_CERT_FILE'] = '/certificate.crt'
+
 lock = threading.Lock()
 
-DS_AGG = {}
- 
+# THIS HAS TO COME AFTER SSL THING
+import pyarrow.parquet as pq
+from pyarrow import fs, Table
+
+logger.info(f"Writing to: {s3_endpoint}")
+minio = fs.S3FileSystem(scheme="https", access_key=s3_access_key, secret_key=s3_secret_key, endpoint_override=s3_endpoint)
+
+logger.info(minio.get_file_info("staging"))
+
+@app.route('/')
+def index():
+    return "Hello from Luna Waystation"
+
+
 @app.route('/datasets/<string:dsid>/segments/<string:sid>', methods=['POST'])
 def post_dataset_segment(dsid, sid):
-    print (f"Recieved data for dataset={dsid}, segment={sid}")
-
-    file = request.files['segment']
-    data = pd.read_csv(file)
+    segment = request.files['segment']
+    data = pd.read_csv(segment)
     data['SEGMENT_ID'] = sid
 
     data = data.set_index('SEGMENT_ID')
-
-    ds_dir = os.path.join(root_data_dir, dsid)
+    logger.info (f"Recieved {len(data)} rows of data for dataset={dsid}, segment={sid}")
+    ds_dir = os.path.join(root_data_dir, dsid, 'data.parquet')
     
     with lock:
-        if not os.path.exists(ds_dir): 
-            os.makedirs(ds_dir)
+        try:
+            df = pq.read_table(ds_dir, filesystem=minio).to_pandas()
+        except:
             df = data
-            df.to_parquet(os.path.join(ds_dir, 'data.parquet'))
         else:
-            df = pd.read_parquet(os.path.join(ds_dir, 'data.parquet'), engine='fastparquet')
-
             if not df.index.name == 'SEGMENT_ID': df = df.set_index("SEGMENT_ID")
-
-            df = df.drop(index=sid, errors='ignore').append(data)
-
-            df.to_parquet(os.path.join(ds_dir, 'data.parquet'))
+            df = pd.concat((df.drop(index=sid, errors='ignore'), data))
+        finally:
+            pq.write_table( Table.from_pandas(df), ds_dir, filesystem=minio)
     
-    print (f"Current dataset length={len(df)}")
+    logger.info (f"Current dataset length={len(df)}")
 
     return {'status':'success', 'dsid':dsid, 'sid':sid, 'rows_written': len(data)}
 
 @click.command()
 def main():
 
-    app.run(host='0.0.0.0',port=6077, threaded=True, debug=True)
+    app.run(host='0.0.0.0',port=6077, threaded=True, debug=False)
 
 if __name__ == '__main__':
     main()
